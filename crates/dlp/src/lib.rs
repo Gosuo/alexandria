53b6f73819f6f425918362f66c7a1c52a4d9f5c3
Jan Tebernum <jan.tebernum@gmail.com>
2023-05-06 23:01:10 +0200

    More structure

---
+++ crates/dlp/src/lib.rs
@@ -0,0 +1,26 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::process::Command;
+
+pub struct YtDlp {
+    cmd: Command,
+}
+
+impl Default for YtDlp {
+    fn default() -> Self {
+        Self { cmd: Command::new("yt-dlp") }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_command() {
+        let dlp = YtDlp::default();
+
+        assert_eq!(dlp.cmd.get_program().to_str().unwrap(), "yt-dlp");
+    }
+}
