bf6c712e35d9ae9c62b259580f009ebb564187d3
Jan Tebernum <jan.tebernum@gmail.com>
2023-05-07 22:26:07 +0200

    First stuff

--- crates/dlp/src/lib.rs
+++ crates/dlp/src/lib.rs
@@ -1,17 +1,68 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::io::Result as IoResult;
 use std::process::Command;
 
-pub struct YtDlp {
+use model::JsonOutput;
+use serde_json::Value;
+
+mod model;
+
+pub struct YoutubeQuery {
     cmd: Command,
 }
 
-impl Default for YtDlp {
+impl Default for YoutubeQuery {
     fn default() -> Self {
-        Self {
-            cmd: Command::new("yt-dlp"),
-        }
+        let mut cmd = Command::new("yt-dlp");
+        cmd.arg("-J");
+
+        Self { cmd }
+    }
+}
+
+impl YoutubeQuery {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn query(&mut self, url: &str) -> Result<JsonOutput, ()> {
+        self.cmd.arg(url);
+
+        let output = self.cmd.output().unwrap().stdout;
+        let value: Value = serde_json::from_slice(&output).unwrap();
+        let json: JsonOutput = serde_json::from_value(value).unwrap();
+
+        Ok(json)
+    }
+}
+
+pub struct YoutubeDlp {
+    cmd: Command,
+}
+
+impl Default for YoutubeDlp {
+    fn default() -> Self {
+        let cmd = Command::new("yt-dlp");
+
+        Self { cmd }
+    }
+}
+
+impl YoutubeDlp {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn download(mut self, url: &str) -> IoResult<()> {
+        self.cmd.arg(url);
+
+        let output = dbg!(self.cmd).output()?;
+
+        dbg!(output);
+
+        Ok(())
     }
 }
 
@@ -21,7 +72,7 @@ mod tests {
 
     #[test]
     fn default_command() {
-        let dlp = YtDlp::default();
+        let dlp = YoutubeDlp::default();
 
         assert_eq!(dlp.cmd.get_program().to_str().unwrap(), "yt-dlp");
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use dlp::{YoutubeDlp, YoutubeQuery};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, downloading: https://www.youtube.com/watch?v=jNQXAC9IVRw");
+    let query = YoutubeQuery::new()
+        .query("https://www.youtube.com/watch?v=jNQXAC9IVRw")
+        .unwrap();
+
+    println!("Query:\n{:?}", query);
 }
